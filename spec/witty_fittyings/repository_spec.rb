fbabce27fcc2c04d660badc075eb4cf478b82539
moro <moronatural@gmail.com>
2012-05-14 17:30:40 +0900

    more spec

--- spec/witty_fittyings/repository_spec.rb
+++ spec/witty_fittyings/repository_spec.rb
@@ -9,16 +9,28 @@ describe WittyFittings::Repository do
 
   let(:lesson) { Lesson.first }
 
-  before do
-    repository.capture do
-      Lesson.create! name: '1st Lesson'
-    end
-  end
-
   after { Lesson.delete_all }
 
-  specify { repository.records[Lesson].should == Set.new([lesson.id]) }
-  specify { repository.fixtures[Lesson].should == [{'id' => lesson.id, 'name' => lesson.name}] }
+  context 'create lesson simply' do
+    before do
+      repository.capture do
+        l = Lesson.create! name: '1st Lesson'
+      end
+      Lesson.create name: '2nd Lesson' # ignored
+    end
+    specify { repository.records[Lesson].should == Set.new([lesson.id]) }
+    specify { repository.fixtures[Lesson].should == [{'id' => lesson.id, 'name' => lesson.name}] }
+  end
+
+  context 'update lesson after create' do
+    before do
+      repository.capture do
+        l = Lesson.create! name: '1st Lesson'
+        l.update_attributes! name: 'Great Lesson!!!'
+      end
+    end
+    specify { repository.fixtures[Lesson].should == [{'id' => lesson.id, 'name' => 'Great Lesson!!!'}] }
+  end
 
 end
 
