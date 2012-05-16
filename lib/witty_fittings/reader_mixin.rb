949511196eb56342ecbfd35422646e90df54ebe7
moro <moronatural@gmail.com>
2012-05-16 18:54:17 +0900

    good-by constructor mixin methods setup.
    instead, set fittings (data container) reference.

--- lib/witty_fittings/reader_mixin.rb
+++ lib/witty_fittings/reader_mixin.rb
@@ -1,13 +1,30 @@
 module WittyFittings
   class ReaderMixin
-    attr_reader :readers
-    def initialize(readers)
-      @readers = readers
+    attr_reader :readers, :fittings
+    def initialize(fittings)
+      @fittings = fittings
+      @readers = {}
+    end
+
+    def let(k, &prc)
+      @readers[k] = prc
+    end
+
+    def setup_module
+      _f = fittings
+      Module.new do
+        define_method :included do |base|
+          base.before(:each) { _f.setup }
+        end
+      end
     end
 
     def to_module
       origin = self
+      m = setup_module
       Module.new do
+        extend m
+
         define_method(:origin) { origin }
         origin.readers.each do |name, prc|
           define_method(name.to_sym, &prc)
--- spec/witty_fittyings/accessor_mixin_spec.rb
+++ spec/witty_fittyings/accessor_mixin_spec.rb
@@ -3,17 +3,18 @@ require 'witty_fittings/reader_mixin'
 
 describe WittyFittings::ReaderMixin do
   let :accessor_mixin do
-    WittyFittings::ReaderMixin.new({
-      foo: -> { 'a value' },
-    })
+    WittyFittings::ReaderMixin.new(nil)
   end
 
-  let(:a_object) { Object.new }
+  let :a_object do
+    Class.new { def self.before(*ignore); end }.new
+  end
 
   context 'defined foo() method to return "a value"' do
     subject { a_object.foo }
 
     before do
+      accessor_mixin.let(:foo){ 'a value' }
       a_object.singleton_class.send :include, accessor_mixin.to_module
     end
 
